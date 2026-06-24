f81c02b81b9a5611c1976e402b2fd9a8481dae49
Mohammad Tayyab <Mohammad.Tayyab@neustrom.de>
2026-06-24 10:36:41 +0200

    feat: add peak shaving dataframe
    Signed-off-by: Mohammad Tayyab <Mohammad.Tayyab@neustrom.de>

--- src/frequenz/lib/notebooks/reporting/__init__.py
+++ src/frequenz/lib/notebooks/reporting/__init__.py
@@ -2,3 +2,7 @@
 # Copyright © 2025 Frequenz Energy-as-a-Service GmbH
 
 """Initialise the reporting related modules."""
+
+from .data_processing import create_battery_usecase_df, create_energy_report_df
+
+__all__ = ["create_energy_report_df", "create_battery_usecase_df"]
